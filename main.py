0b1461aa38c976e08a10ae6d98c68092ef98915b
Chalac34 <122803250+Chalac34@users.noreply.github.com>
2023-01-23 18:00:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ hostname=socket.gethostname()
 
 ###Titre de la page
 root = tk.Tk()
+root.geometry("725x800+600+100")
 root.title("SemaOS")
 ####
 
@@ -102,6 +103,8 @@ def Scan_port(IP,debut,fin):
     text.tag_config("italic", font=("Sans", 12, "italic"))
 
     i=0
+    TCP=[]
+    UDP=[]
     progress.start()
     for port in range(debut,fin+1):
         i+=1
@@ -115,6 +118,7 @@ def Scan_port(IP,debut,fin):
             text.insert(tk.END, f"{completion}%")
             Template()
             text.insert(tk.END,f"Port {port} est ouvert en TCP.")
+            TCP.append(port)
             root.update()
 
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -122,10 +126,13 @@ def Scan_port(IP,debut,fin):
         result = sock.connect_ex((IP,port))
         if result == 0:
             Template()
-            text.insert(tk.END, f"{completion}%")
-            Template()
             text.insert(tk.END,f"Port {port} est ouvert en UDP.")
+            UDP.append(port)
             root.update()
+    Template()
+    text.insert(tk.END,f"Port {TCP} en TCP.")
+    Template()
+    text.insert(tk.END,f"Port {UDP} en UDP.")
     text.config(state="disable")
     progress.stop()
     sock.close()
@@ -151,42 +158,41 @@ def Speedtest():
     text.tag_config("italic", font=("Sans", 12, "italic"))
 
     Template()
-    text.insert(tk.END,f"{current_time} ","italic")
-    Template()
     progress.start()
-    text.insert(tk.END,"Speedtest en cours,Veuillez patientez... ")
+    text.insert(tk.END," Speedtest en cours,Veuillez patientez... ")
     root.update()
-
+    print("ok1")
     str_DL=""
     str_UP=""
     program_path = "C:\\Users\\b.dezord\\Desktop\\spped\\speedtest.exe"
 
     result = subprocess.run(program_path, shell=True, capture_output=True, text=True)
     output = result.stdout.split('\n')
+    print("ok2")
+    update_progress()
 
     for line in output:
-        update_progress()
         if 'Download:' in line:
              str_DL=line
         if 'Upload:' in line:
              str_UP=line
-
+    print("ok3")
     DL=str_DL.split()[1]+str_DL.split()[2]
     UP=str_UP.split()[1]+str_DL.split()[2]
     progress.stop()
 
     ###Partie du Download
     Template()
-    text.insert(tk.END,f"Vitesse de Download ")
+    text.insert(tk.END,f" Vitesse de Download ")
     Template()
-    text.insert(tk.END,f"{DL}\n", "bold")
+    text.insert(tk.END,f" {DL}\n", "bold")
     root.update()
-
+    print("ok4")
     ###Partie du Upload
     Template()
-    text.insert(tk.END,f"Vitesse D'upload  ")
+    text.insert(tk.END,f" Vitesse D'upload  ")
     Template()
-    text.insert(tk.END,f"{UP}", "bold ")
+    text.insert(tk.END,f" {UP}", "bold ")
     root.update()
     text.config(state="disable")
     log_text()
@@ -207,24 +213,26 @@ def Template():
 
 
 
+
+
 ####LOGO###
 root.wm_iconbitmap("C:\\Users\\b.dezord\\Desktop\\SEMAOS.ico")
 
 ####BOUTONS####
 
 ###Bouton Test###
-bouton_Template= tk.Button(root, text="Template", command=Template, width = 10, height = 5)
+bouton_Template= tk.Button(root, text="Template", command=Template, width = 20, height = 3,bg="#1B97C6", bd=8)
 bouton_Template.pack()
 #bouton_test.place(x=10, y=10)
 
 ####Bouton Scan SPEEDTEST
 
-speedtest_button = tk.Button(root, text="SpeedTest", command=Speedtest, width = 10, height = 5)
+speedtest_button = tk.Button(root, text="SpeedTest", command=Speedtest, width = 20, height = 3,bg="#B991A3", bd=8)
 speedtest_button.pack()
 #speedtest_button.place(x=120, y=10)
 #####
 ###Bouton Scan_Port
-scan_button = tk.Button(root, text="Scan Ports", command=on_scan_ports, width = 10, height = 5)
+scan_button = tk.Button(root, text="Scan Ports", command=on_scan_ports, width = 20, height = 3,bg="#1B97C6", bd=8)
 scan_button.pack()
 #scan_button.place(x=230, y=10)
 ###
@@ -232,7 +240,7 @@ scan_button.pack()
 
 ####Bouton Scan IP
 
-scan_IP_button = tk.Button(root, text="Scan IP", command=on_scan_ip, width = 10, height = 5)
+scan_IP_button = tk.Button(root, text="Scan IP", command=on_scan_ip, width = 20, height = 3,bg="#B991A3", bd=8)
 scan_IP_button.pack()
 #scan_IP_button.place(x=340, y=10)
 ########
@@ -242,6 +250,7 @@ scan_IP_button.pack()
 text = tk.Text(root)
 text.config(state="disable")
 text.config(bg='#1e1e29', fg='#e0e1e6')
+text.config(width=100,height=50)
 text.pack()
 #####
 
